a8fdca15aa1a78999bba0fc096f6fb01eb3df50f
Evan Carlin <evan@carlin.com>
2014-11-13 21:25:58 -0400

    Started work on some html/css styling.

--- peeralliesapp/app/models/peer_ally.rb
+++ peeralliesapp/app/models/peer_ally.rb
@@ -1,7 +1,7 @@
 class PeerAlly < ActiveRecord::Base
   attr_accessible :name, :grade, :bio, :avatar
 
-  has_attached_file :avatar, :styles => {:thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => {:thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
