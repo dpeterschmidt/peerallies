bf493bc87f016db7c1156314c5d47d70224bc006
Beth Anderson <bmanderson@pugetsound.edu>
2014-11-23 14:47:58 -0800

    Fixed routing errors, updated nav. bar

--- peeralliesapp/config/routes.rb
+++ peeralliesapp/config/routes.rb
@@ -23,6 +23,7 @@ Peeralliesapp::Application.routes.draw do
   match :conf_yes, :to => 'peer_allies#conf_yes'
 
   match :survey, :to => 'peer_allies#survey'
+  
 
   post "/peer_allies/new" => 'peer_allies#create'
 
