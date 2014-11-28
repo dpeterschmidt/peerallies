e78c8c1408bad339ec99c310a8b207d8c0892734
Joshua Nance <jnance@pugetsound.edu>
2014-11-27 20:51:27 -0800

    format email form

--- peeralliesapp/spec/mailers/ally_mailer_spec.rb
+++ peeralliesapp/spec/mailers/ally_mailer_spec.rb
@@ -1,18 +1,37 @@
-require "rails_helper"
+require "spec_helper"
 
 RSpec.describe AllyMailer, :type => :mailer do
   describe "notification_email" do
     let(:mail) { AllyMailer.notification_email }
-
-    it "renders the headers" do
-      expect(mail.subject).to eq("Notification email")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
-    end
-
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
-    end
   end
 
+  before(:each) do
+    ActionMailer::Base.delivery_method = :test
+    ActionMailer::Base.perform_deliveries = true
+    ActionMailer::Base.deliveries = []
+    @name = "Joshua Nance"
+    @email = 'jnance@pugetsound.edu'
+    @message = "Message received"
+    AllyMailer.notification_email(@name, @email, @message).deliver
+  end
+
+  after(:each) do
+    ActionMailer::Base.deliveries.clear
+  end
+
+  it 'should send an email' do
+    # ActionMailer::Base.deliveries.count.should == 1
+    expect(ActionMailer::Base.deliveries.count).to eq(1)
+
+  end
+
+  it 'renders the receiver email' do
+    # ActionMailer::Base.deliveries.first.to.should == @email
+    expect(ActionMailer::Base.deliveries.second).to eq(@email)
+  end
+
+  it 'renders the sender email' do
+    # ActionMailer::Base.deliveries.first.from.should == "peerallies.pugetsound.edu"
+    expect(ActionMailer::Base.deliveries.first.from).to eq(@email);
+  end
 end
