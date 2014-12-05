b93655289465b856d55c44e37c217d0a5a156f14
Joshua Nance <jnance@pugetsound.edu>
2014-12-05 13:18:46 -0800

    commented out pg gem + commented ally_mailer.rb

--- peeralliesapp/app/mailers/ally_mailer.rb
+++ peeralliesapp/app/mailers/ally_mailer.rb
@@ -1,14 +1,31 @@
 class AllyMailer < ActionMailer::Base
 
-  default to: "peerallies.pugetsound@gmail.com"
+  # the default address to send a confirmation_email from
+  default from: "peerallies.pugetsound@gmail.com"
 
+  # this is the email notifying an ally that a victim has requested
+  # help from the peer allies
+  # Params:
+  # +name+:: the name of the victim
+  # +email+:: the email address of the victim
+  # +message+:: the message the victim wishes to include
   def notification_email(name, email, message)
+    # auto includes the name in an opening sentence to introduce
+    # the victim to the ally
     body = "Hi, my name is #{name}. " + "\n" + "\n" + message
     mail from: email, body: body
   end
 
+  # this is the email from the peer allies to the victim
+  # providing confirmation that they received a request
+  # Params:
+  # +name+:: The name of the victim
+  # +email+:: The email address of the victim
   def confirmation_email(name, email)
-    body = "Hello, #{name}." + "\n" + "The Peer Allies have received your request, and will get in touch with you shortly."
+    body = "Hello, #{name}."  + "\n" + "The Peer Allies " +
+      "have received your request, and will get in " +
+      "touch with you as soon as possible."
+    
     mail to: email, from: "peerallies.pugetsound@gmail.com", body: body
   end
 
