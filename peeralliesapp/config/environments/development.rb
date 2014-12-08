688279121b14a1c2c88fd17226854c8827afe81b
Evan Carlin <evan@carlin.com>
2014-12-08 02:13:44 -0400

    Avatar fix

--- peeralliesapp/config/environments/development.rb
+++ peeralliesapp/config/environments/development.rb
@@ -58,9 +58,5 @@ Peeralliesapp::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-
-  # Paperclip config:
-  Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
-  Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
 end
 
--- peeralliesapp/config/environments/production.rb
+++ peeralliesapp/config/environments/production.rb
@@ -88,8 +88,4 @@ Peeralliesapp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  # Paperclip config:
-  Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
-  Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
 end
