3bedcc076a87dc0674e5b4e737dd6b4389bbb55e
Evan Carlin <evan@carlin.com>
2014-12-10 17:55:19 -0400

    Changed config files to allow login through Heroku.

--- peeralliesapp/config/initializers/rack_saml.rb
+++ peeralliesapp/config/initializers/rack_saml.rb
@@ -1,5 +1,13 @@
-Rails.application.config.middleware.insert_after Rack::ETag, Rack::Saml, {
-	:config => "#{Rails.root}/config/rack-saml.yml",
-	:metadata => "#{Rails.root}/config/metadata.yml",
-	:attribute_map => "#{Rails.root}/config/attribute-map.yml"
-}
+if Rails.env.production? #production use project-specific confi
+	Rails.application.config.middleware.insert_after Rack::ETag, Rack::Saml, {
+		:config => "#{Rails.root}/config/rack-saml-production.yml",
+		:metadata => "#{Rails.root}/config/metadata.yml",
+		:attribute_map => "#{Rails.root}/config/attribute-map.yml"
+	}
+else #development or test use normal config 
+		Rails.application.config.middleware.insert_after Rack::ETag, Rack::Saml, {
+		:config => "#{Rails.root}/config/rack-saml.yml",
+		:metadata => "#{Rails.root}/config/metadata.yml",
+		:attribute_map => "#{Rails.root}/config/attribute-map.yml"
+	}
+end
