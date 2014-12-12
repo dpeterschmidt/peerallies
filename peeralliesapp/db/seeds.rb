a8adc9ec4de06fa6798c60ee2662253547358186
Evan Carlin <evan@carlin.com>
2014-12-12 17:37:55 -0400

    Made our profiles visible.

--- peeralliesapp/db/seeds.rb
+++ peeralliesapp/db/seeds.rb
@@ -8,10 +8,8 @@
 
 #seed yourselves here
 
-PeerAlly.create(name: 'Daniel Peterschmidt', bio: "Seeded developer Daniel Peterschmidt", ups_id: 'dpeterschmidt', :administrator => true, :display => false)
-PeerAlly.create(name: 'Evan Carlin', bio: "Seeded developer Evan Carlin", ups_id: 'ecarlin', :administrator => true, :display => false)
-PeerAlly.create(name: 'Beth Anderson', bio: "Seeded developer Beth Anderson", ups_id: 'bmanderson', :administrator => true, :display => false)
-PeerAlly.create(name: 'Joshua Nance', bio: "Seeded developer Joshua Nance", ups_id: 'jnance', :administrator => true, :display => false)
-PeerAlly.create(name: 'Jacob Imlay', bio: "Seeded developer Jacob Imlay", ups_id: 'jimlay', :administrator => true, :display => false)
-
-PeerAlly.create(name: 'Peer Ally #1', bio: "Ally #1", ups_id: 'ally1', :display => true)
+PeerAlly.create(name: 'Daniel Peterschmidt', bio: "Seeded developer Daniel Peterschmidt", ups_id: 'dpeterschmidt', :administrator => true, :display => true)
+PeerAlly.create(name: 'Evan Carlin', bio: "Seeded developer Evan Carlin", ups_id: 'ecarlin', :administrator => true, :display => true)
+PeerAlly.create(name: 'Beth Anderson', bio: "Seeded developer Beth Anderson", ups_id: 'bmanderson', :administrator => true, :display => true)
+PeerAlly.create(name: 'Joshua Nance', bio: "Seeded developer Joshua Nance", ups_id: 'jnance', :administrator => true, :display => true)
+PeerAlly.create(name: 'Jacob Imlay', bio: "Seeded developer Jacob Imlay", ups_id: 'jimlay', :administrator => true, :display => true)
